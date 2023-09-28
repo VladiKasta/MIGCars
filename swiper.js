41a26e17ecc388554f5b6df82da76848111be6f4
VladiKasta <voroshilov2003@mail.ru>
2023-09-28 12:20:57 +0300

    about us mobile

--- swiper.js
+++ swiper.js
@@ -76,28 +76,50 @@ const swiperCategory = new Swiper(".swiper_category", {
 
 const swiperBrands = new Swiper(".about_us_swiper", {
   // Optional parameters
-  /* autoHeight: true, */
-  slidesPerView: 5.5,
+  autoHeight: true,
+  /* slidesPerView: 2.5, */
   spaceBetween: 10,
 
   loop: true,
 
-  loop: true,
   autoplay: {
-    delay: 3000, // задержка между переключениями в миллисекундах
+    delay: 1000, // задержка между переключениями в миллисекундах
   },
 
   // If we need pagination
-  pagination: {
-    el: ".swiper-pagination",
-    dynamicBullets: true,
-    clickable: true,
-  },
 
-  // Navigation arrows
-  navigation: {
-    nextEl: ".swiper-button-next",
-    prevEl: ".swiper-button-prev",
+  breakpoints: {
+    // when window width is >= 320px
+
+    320: {
+      slidesPerView: 1.5,
+      spaceBetween: 5,
+    },
+
+    480: {
+      slidesPerView: 2.5,
+      spaceBetween: 5,
+    },
+
+    768: {
+      slidesPerView: 3,
+      spaceBetween: 5,
+    },
+
+    1050: {
+      slidesPerView: 4,
+      spaceBetween: 15,
+    },
+    // when window width is >= 480px
+    1300: {
+      slidesPerView: 4,
+      spaceBetween: 30,
+    },
+    // when window width is >= 640px
+    1520: {
+      slidesPerView: 6,
+      spaceBetween: 12,
+    },
   },
 
   // And if we need scrollbar
